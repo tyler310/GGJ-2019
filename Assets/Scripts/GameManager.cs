ab91bd1df7fbbd438792d64940db01780adfbbf9
tyler310 <tyler0310@gmail.com>
2019-01-27 00:14:27 -0800

    We can stack some stuffs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	[SerializeField] private GameObject go;
+	[SerializeField] private Canvas canvas;
+	[SerializeField] private Camera mainCamera;
+	[SerializeField] private float offsetValue;
+	
+	private Vector3 offset;
+	void Awake()
+	{
+		offset = new Vector3(0, offsetValue, 0);	
+	}
+	
+	void Update ()
+	{
+		Vector2 spawnPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+//		Vector2 spawnPos = Input.mousePosition;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			GameObject block = Instantiate(go, spawnPos, Quaternion.identity);
+		}
+//		MoveCamera();
+	}
+
+	private void MoveCamera()
+	{
+		mainCamera.transform.position = mainCamera.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/MaxHeightTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MaxHeightTrigger : MonoBehaviour
+{
+	public Camera mainCamera;
+	public float offset;
+	public float panTime;
+
+	private bool isTriggered;
+
+	void Awake()
+	{
+		isTriggered = false;
+	}
+	
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Stack") && !isTriggered)
+		{
+			Debug.Log("Triggered");
+			isTriggered = true;
+			Vector3 panTo = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y + offset, -1);
+			mainCamera.transform.DOMove(panTo, panTime).SetEase(Ease.Linear).OnComplete(() => isTriggered = false);
+		}
+	}
+}
