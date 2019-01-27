5ccac02284f55d3cb95d5d56dd7ab7c9a7044557
t6f1b <yixujoy@gmail.com>
2019-01-26 20:08:17 -0800

    added top dimsum part

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+	public GameObject dumpling;
+	public GameObject spawnPoint;
+	public Canvas canvas;
+
+	private float floor = 0;
+
+	// Use this for initialization
+	void Start () {
+		Vector2 point = spawnPoint.transform.position;
+		Instantiate(dumpling, point, transform.rotation, canvas.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector2 point = spawnPoint.transform.position;
+			Instantiate(dumpling, point, transform.rotation, canvas.transform);
+		}
+	}
+}
